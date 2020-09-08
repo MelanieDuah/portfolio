31572b44a59d04325ba671eccb1cbf8a7de1488e
melanie <melanieduah@gmail.com>
2020-09-08 00:29:05 -0500

    Updates

---
+++ precache-manifest.3e22d83ec6b273f601db342a0e838b06.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86dbf35f875a464ef511e32093261fc3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "46c6e9e83d64942b25eb",
+    "url": "/portfolio/static/css/main.958d6073.chunk.css"
+  },
+  {
+    "revision": "088baf98edba4ef23d42",
+    "url": "/portfolio/static/js/2.705a83e6.chunk.js"
+  },
+  {
+    "revision": "aa14ee2d62c93cfe84a4720fac3ca556",
+    "url": "/portfolio/static/js/2.705a83e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c6e9e83d64942b25eb",
+    "url": "/portfolio/static/js/main.445a6dba.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5cea03bc515a74653846d915eab6a918",
+    "url": "/portfolio/static/media/burger.5cea03bc.png"
+  },
+  {
+    "revision": "7c0999e529941d8c98b0a06bf162d741",
+    "url": "/portfolio/static/media/daylytracker.7c0999e5.png"
+  },
+  {
+    "revision": "5198e1717133231ccf1b75ebcf6d90d2",
+    "url": "/portfolio/static/media/googlebook.5198e171.png"
+  },
+  {
+    "revision": "ccd30b53b8dc5e2514002ffbd04c1024",
+    "url": "/portfolio/static/media/hero.ccd30b53.jpg"
+  },
+  {
+    "revision": "29e1f680dc7d6b0670f0efc9f46d7f87",
+    "url": "/portfolio/static/media/hero2.29e1f680.png"
+  },
+  {
+    "revision": "3bd54d98739fe5512e1a6c12a2f95f45",
+    "url": "/portfolio/static/media/image2.3bd54d98.png"
+  },
+  {
+    "revision": "716e1c2521b573e50ccfdf8dd288e5c6",
+    "url": "/portfolio/static/media/image7.716e1c25.jpg"
+  },
+  {
+    "revision": "b7e3691273b011dbc509552cf2b3905d",
+    "url": "/portfolio/static/media/notetaker.b7e36912.png"
+  },
+  {
+    "revision": "df5cf8d64b0907a98b9e7ea4f8a1498c",
+    "url": "/portfolio/static/media/password.df5cf8d6.png"
+  },
+  {
+    "revision": "d741f7669a29dfcf4c5d8fbdc94dc2d6",
+    "url": "/portfolio/static/media/quiz.d741f766.png"
+  },
+  {
+    "revision": "c4ebc80c7422b8e64c47a976474225c9",
+    "url": "/portfolio/static/media/transaction.c4ebc80c.png"
+  },
+  {
+    "revision": "b5b71a662fe442826ec80cd8f35a406d",
+    "url": "/portfolio/static/media/weather.b5b71a66.png"
+  }
+]);
