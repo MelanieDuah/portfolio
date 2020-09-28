a2cf986673c846227c848168e3ab0031f261433b
melanie <melanieduah@gmail.com>
2020-09-28 17:25:53 -0500

    Updates

---
+++ precache-manifest.3ac8223f5feea8c590603dd581c141b8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb5553af4e57d001b3db3232706582c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6582705049958e4adb6",
+    "url": "/static/css/main.845be5e9.chunk.css"
+  },
+  {
+    "revision": "ed0346cfbf4f111d5ac3",
+    "url": "/static/js/2.ff57ede9.chunk.js"
+  },
+  {
+    "revision": "aa14ee2d62c93cfe84a4720fac3ca556",
+    "url": "/static/js/2.ff57ede9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6582705049958e4adb6",
+    "url": "/static/js/main.a1ad2a71.chunk.js"
+  },
+  {
+    "revision": "07114ff5235e0e9725f4",
+    "url": "/static/js/runtime-main.87d96846.js"
+  },
+  {
+    "revision": "7c0999e529941d8c98b0a06bf162d741",
+    "url": "/static/media/daylytracker.7c0999e5.png"
+  },
+  {
+    "revision": "ccd30b53b8dc5e2514002ffbd04c1024",
+    "url": "/static/media/hero.ccd30b53.jpg"
+  },
+  {
+    "revision": "29e1f680dc7d6b0670f0efc9f46d7f87",
+    "url": "/static/media/hero2.29e1f680.png"
+  },
+  {
+    "revision": "3bd54d98739fe5512e1a6c12a2f95f45",
+    "url": "/static/media/image2.3bd54d98.png"
+  },
+  {
+    "revision": "716e1c2521b573e50ccfdf8dd288e5c6",
+    "url": "/static/media/image7.716e1c25.jpg"
+  },
+  {
+    "revision": "b7e3691273b011dbc509552cf2b3905d",
+    "url": "/static/media/notetaker.b7e36912.png"
+  },
+  {
+    "revision": "df5cf8d64b0907a98b9e7ea4f8a1498c",
+    "url": "/static/media/password.df5cf8d6.png"
+  },
+  {
+    "revision": "d741f7669a29dfcf4c5d8fbdc94dc2d6",
+    "url": "/static/media/quiz.d741f766.png"
+  },
+  {
+    "revision": "c4ebc80c7422b8e64c47a976474225c9",
+    "url": "/static/media/transaction.c4ebc80c.png"
+  },
+  {
+    "revision": "b5b71a662fe442826ec80cd8f35a406d",
+    "url": "/static/media/weather.b5b71a66.png"
+  }
+]);
